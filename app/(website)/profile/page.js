57f7e79102e1f0b99c4298fe6892863ed9b947c9
Martí Arnau <marnauortega@gmail.com>
2023-06-19 03:40:49 +0200

    feat(profile page)

--- app/(website)/profile/page.js
+++ app/(website)/profile/page.js
@@ -1,20 +1,12 @@
+import { useContext } from "react";
+import { UserContext } from "@/components/Providers/UserProvider";
+
 import Header from "@/components/Header/Header";
 import UserForm from "@/components/UserForm/UserForm";
 
 import styles from "./page.module.css";
 
 const Profile = () => {
-  const { user } = useContext(UserContext);
-
-  useEffect(() => {
-    const userJSON = localStorage.getItem("user");
-
-    if (userJSON) {
-      const user = JSON.parse(userJSON);
-      setUser(user);
-    }
-  }, []);
-
   return (
     <>
       <Header />
